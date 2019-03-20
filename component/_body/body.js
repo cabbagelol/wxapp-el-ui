8af17a60e4b35e5c97e23ca553196dc13d7839db
cabbagelol <nickmiku@foxmail.com>
2019-03-20 11:30:13 +0800

    2019-3-20 11:28:51

--- component/_body/body.js
+++ component/_body/body.js
@@ -5,6 +5,30 @@ Component({
     multipleSlots: true
   },
 
+  properties: {
+    animation: Number,
+    scrollY: {
+      type: Number,
+      value: 0
+    },
+    fullscreem: Boolean,
+    rubber: Boolean,
+    upperThreshold: {
+      type: Number,
+      value: 0
+    },
+    lowerThreshold: {
+      type: Number,
+      value: 0
+    }
+  },
+
+  observers: {
+    'scrollY': function(newNumber_) {
+      this.setSrcollData(newNumber_, this.data.animation || 200)
+    }
+  },
+
   data: {
     content: {
       head: {
@@ -14,6 +38,7 @@ Component({
         height: 0,
         tap: {
           top: 0,
+          end: 0,
           duration: 0
         },
         view: {
@@ -23,15 +48,13 @@ Component({
       footer: {
         height: 0
       }
-    },
-    test: 0
+    }
   },
 
   ready() {
     var that = this;
     var info = wx.getSystemInfoSync();
-
-    if (config.util.in(this)) {
+    if (config.util.in(that)) {
       config.util.$('.__content-head__').then(function(e) {
         that.setData({
           'content.head.height': e.height
@@ -51,6 +74,16 @@ Component({
         that.setData({
           'content.body.height': info.screenHeight - that.data.content.head.height - that.data.content.footer.height
         })
+        that.srcollHead = -(that.data.content.body.view.height - that.data.content.body.height);
+        that.triggerEvent('ready', {
+          body: {
+            head: that.data.content.head.height,
+            body: that.data.content.body.height,
+            footer: that.data.content.footer.height,
+            scrollButtonY: that.srcollHead
+          },
+          scrollY: that.data.content.body.tap.top
+        })
       })
     }
   },
@@ -58,79 +91,55 @@ Component({
   methods: {
     onBodyStart(e) {
       var that = this
-      if (e.changedTouches[0].length <= 1) { return }
+      if (e.changedTouches[0].length <= 1) {
+        return
+      }
       this.setData({
         'content.body.tap.starttime': new Date().getTime(),
         'content.body.tap.start': e.changedTouches[0].pageY,
-        'content.body.tap.duration': 0
+        'content.body.tap.duration': 0,
+        'content.body.tap.end': this.data.content.body.tap.top
       })
     },
 
     onBodyEnd(e) {
-      var that = this
-
-      // var speed = (e.changedTouches[0].pageY - that.data.content.body.tap.start) / (that.data.content.body.tap.starttime - new Date().getTime())
-      // var time = setInterval(function () {
-      //   speed = speed - 0.1;
-      //   that.setData({
-      //     'content.body.tap.top': that.data.content.body.tap.top - speed,
-      //     'content.body.tap.duration': 200
-      //   })
-      //   if (speed <= 0) { clearInterval(time) }
-      // }, 20)
+      var that = this;
       if (that.data.content.body.tap.top >= 0) {
-        that.setData({
-          'content.body.tap.top': 0,
-          'content.body.tap.duration': 200
-        })
-      } else if (that.data.content.body.tap.top <= -(that.data.content.body.view.height - that.data.content.body.height)) {
-        that.setData({
-          'content.body.tap.top': -(that.data.content.body.view.height - that.data.content.body.height),
-          'content.body.tap.duration': 200
-        })
-      }
-      that.setData({
-        test: that.data.content.body.tap.top
-      })
-      if (that.data.content.body.tap.top > 0){
-        that.setData({
-          'content.body.tap.top': 0,
-          test: 0
-        })
+        that.setSrcollData(0, that.data.animation || 200)
+      } else if (that.data.content.body.tap.top <= that.srcollHead) {
+        that.setSrcollData(that.srcollHead > 0 ? 0 : that.srcollHead, that.data.animation || 200)
       }
     },
-
-    y: 0,
 
     onBodyMove(e) {
       var that = this;
-      var point = e.changedTouches[0],
-        calcY = point.pageY - that.data.content.body.tap.start + that.data.test
-      if (that.data.content.body.tap.top >= 100) { return }
-      that.setData({
-        'content.body.tap.top': calcY
+      var point = e.changedTouches[0]
+      var calcY = point.pageY - that.data.content.body.tap.start + that.data.content.body.tap.end;
+      if (that.data.upperThreshold && calcY > that.data.upperThreshold) {
+        calcY = that.data.upperThreshold
+      } else if (that.data.lowerThreshold > 0 && calcY < that.srcollHead - that.data.lowerThreshold) {
+        calcY = that.srcollHead - that.data.lowerThreshold
+      }
+      if (!that.data.rubber && calcY >= 0) {
+        calcY = 0
+      } else if (!that.data.rubber && calcY < that.srcollHead) {
+        calcY = that.srcollHead
+      }
+      that.setSrcollData(calcY, 0)
+      that.getSrcollData()
+    },
+
+    getSrcollData() {
+      this.triggerEvent('scroll', {
+        scrollY: this.data.content.body.tap.top
       })
-      // if (calcY > 0){
-      //   that.setData({
-      //     'content.body.tap.top': 0
-      //   })
-      // }
-      // if (that.data.content.body.tap.top > 0) {
-      //   that.setData({
-      //     'content.body.tap.top': 0
-      //   })
-      // }
     },
 
-    oncalc (val_) {
-      var val = val_;
-      console.log(val)
-      return val
+    setSrcollData(number_, time_) {
+      this.setData({
+        'content.body.tap.top': number_ || 0,
+        'content.body.tap.duration': time_ || 0,
+      })
     },
-
-    isNumber(old_, new_) {
-      return new_ > old_ ? true : false 
-    },
-
   }
 })
