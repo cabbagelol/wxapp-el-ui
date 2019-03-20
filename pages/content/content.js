4605953bd471ca4864c0fd5d558cc5594b810eee
cabbagelol <nickmiku@foxmail.com>
2019-03-20 08:17:28 +0800

    主支

--- pages/content/content.js
+++ pages/content/content.js
@@ -4,7 +4,50 @@ Page({
       title: '主体',
       back: true
     },
+    content: {
+      head: 0,
+      body: 0,
+      footer: 0,
+      upperThreshold: 100,
+      lowerThreshold: 100,
+      autoFullscreenf: true,
+      rubber: true,
+      y: 0,
+      tap: {
+        y: 0
+      }
+    }
   },
 
+  onScroll (e) {
+    console.log(e)
+    this.setData({
+      'content.y': e
+    })
+  },
 
-})
+  onScrollReady (e) {
+    this.setData({
+      'content.head': e.detail.body.head,
+      'content.body': e.detail.body.body,
+      'content.footer': e.detail.body.footer
+    })
+  },
+
+  onSetScroll (e) {
+    this.setData({
+      'content.y': e.detail.value
+    })
+  },
+  onInput(e) {
+    this.setData({
+      [e.target.dataset.e]: e.detail.value
+    })
+  },
+
+  onSwitch(e) {
+    this.setData({
+      [e.target.dataset.e]: e.detail.value
+    })
+  }
+})  
