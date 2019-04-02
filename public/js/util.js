ce686aa9c434f4b5049a27bbcdb580ad9bff3e12
cabbagelol <nickmiku@foxmail.com>
2019-04-02 10:21:53 +0800

    2019-4-2 10:21:50

---
+++ public/js/util.js
@@ -0,0 +1,15 @@
+module.exports = {
+  $(e) {
+    try {
+      if (!!e.target.dataset.e == false) {
+        throw 'The field "data-e" was not found.';
+        return;
+      }
+      this.setData({
+        [e.target.dataset.e]: e.detail.value
+      })
+    } catch (E) {
+      console.error(E)
+    }
+  }
+}
