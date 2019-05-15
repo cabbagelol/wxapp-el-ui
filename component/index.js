7acbe8be8c8e935e0d984296f99d9028aafc8bdd
cabbagelol <nickmiku@foxmail.com>
2019-05-15 10:01:57 +0800

    2019-5-15 10:01:53

--- component/index.js
+++ component/index.js
@@ -1,56 +1,28 @@
 module.exports = {
   util: {
-    that: null,
-
-    $(name_) {
-      var $self = this;
-      var $res = new Object({
-        self: $self.that
-      });
-      return new Promise(function (resolve, reject) {
-        wx.createSelectorQuery().in($self.that).selectAll(String(name_)).boundingClientRect(function (e) {
-          e.forEach(function (i, index) {
+    $fields(name_, d_) {
+      var that = this;
+      var res = {};
+      return new Promise(function(resolve, reject) {
+        wx.createSelectorQuery().in(that).selectAll(String(name_)).fields(Object.assign({
+          id: true
+        }, d_), function(e) {
+          e.forEach(function(i, index) {
             if (typeof name_ === 'string' && name_.split(',').length == 1 && e.length > 1) {
-              $res[`${name_}${index}`] = i;
-              $res.length = e.length;
+              res[`${name_}${index}`] = i;
+              res.length = e.length;
             } else if (typeof name_ === 'string' && name_.split(',').length == e.length) {
-              $res[name_.split(',').reverse()[index]] = i;
-              $res.length = e.length;
+              // console.log(index, i, name_, e[index]['id'])
+              res[e[index]['id']] = i;
+              res.length = e.length;
             } else {
-              $res[(e.length > 1 ? name_[index] : name_)] = i;
-              $res.length = e.length;
+              res[(e.length > 1 ? name_[index] : name_)] = i;
+              res.length = e.length;
             }
           })
-          resolve($res);
+          resolve(res);
         }).exec();
       })
-    },
-
-    $S(name_, callback) {
-      var $self = this;
-      var $res = new Object({
-        self: $self.that
-      });
-      wx.createSelectorQuery().in($self.that).selectAll(String(name_)).boundingClientRect(function (e) {
-        e.forEach(function (i, index) {
-          if (typeof name_ === 'string' && name_.split(',').length == 1 && e.length > 1) {
-            $res[`${name_}${index}`] = i;
-            $res.length = e.length;
-          } else if (typeof name_ === 'string' && name_.split(',').length == e.length) {
-            $res[name_.split(',').reverse()[index]] = i;
-            $res.length = e.length;
-          } else {
-            $res[(e.length > 1 ? name_[index] : name_)] = i;
-            $res.length = e.length;
-          }
-        })
-        callback($res);
-      }).exec();
-    },
-
-    in(that_) {
-      this.that = that_;
-      return this.that ? true : false
     },
   },
 }
