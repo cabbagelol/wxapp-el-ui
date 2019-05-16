4352ff762532270e1a8b567dde1d6b7ec6c4c027
cabbagelol <nickmiku@foxmail.com>
2019-05-16 18:02:54 +0800

    2019-5-16 18:02:48

---
+++ pages/divider/divider.js
@@ -0,0 +1,12 @@
+Page({
+  data: {
+    head: {
+      title: '分割线',
+      style: {
+        background: 'linear-gradient(45deg, rgba(11, 156, 167, 0.89), rgba(24, 182, 193, 0.87))',
+        textColor: '#FFF'
+      },
+      back: true
+    },
+  },
+})
